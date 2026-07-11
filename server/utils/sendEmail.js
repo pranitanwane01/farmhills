302d3b8b7937aa2106b79eceeaf3e4fa77150a87
PRANIT ANWANE <pranitanwane@PRANITs-MacBook-Air.local>
2026-07-11 23:09:53 +0530

    uuu

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,53 +1,27 @@
-// const nodemailer = require("nodemailer");
-
-// const sendEmail = async (to, subject, text) => {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-
-//     auth: {
-//       user: process.env.EMAIL_USER,
-
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-
-//   await transporter.sendMail({
-//     from: process.env.EMAIL_USER,
-
-//     to,
-
-//     subject,
-
-//     text,
-//   });
-// };
-
-// module.exports = sendEmail;
-
-
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-
-  family: 4,
-});
-
 const sendEmail = async (to, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+
+    auth: {
+      user: process.env.EMAIL_USER,
+
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
+
     to,
+
     subject,
+
     text,
   });
 };
 
 module.exports = sendEmail;
+
+
