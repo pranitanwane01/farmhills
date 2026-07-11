db94e3bd12a154deec3f35e55867db0a9dad8085
PRANIT ANWANE <pranitanwane@PRANITs-MacBook-Air.local>
2026-07-11 22:04:16 +0530

    Fix Gmail SMTP

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -27,37 +27,27 @@
 
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+
+  family: 4,
+});
+
 const sendEmail = async (to, subject, text) => {
-  try {
-    console.log("Creating transporter...");
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    console.log("Verifying transporter...");
-    await transporter.verify();
-    console.log("Transport verified.");
-
-    console.log("Sending email...");
-
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      text,
-    });
-
-    console.log("Email sent:", info.response);
-
-  } catch (err) {
-    console.error("EMAIL ERROR:", err);
-    throw err;
-  }
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  });
 };
 
 module.exports = sendEmail;
