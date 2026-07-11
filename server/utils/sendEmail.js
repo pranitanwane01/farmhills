c0aa3d7a8dfd268da0ea533296106940d16032a7
PRANIT ANWANE <pranitanwane@PRANITs-MacBook-Air.local>
2026-07-11 23:18:50 +0530

    some

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,27 +1,58 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (to, subject, text) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+
+//     auth: {
+//       user: process.env.EMAIL_USER,
+
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+
+//     to,
+
+//     subject,
+
+//     text,
+//   });
+// };
+
+// module.exports = sendEmail;
+
+
+
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
 
     auth: {
-      user: process.env.EMAIL_USER,
-
-      pass: process.env.EMAIL_PASS,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     },
-  });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
 
-    to,
+const sendEmail = async (to, subject, text) => {
+    await transporter.verify();
+    console.log("SMTP Connected");
 
-    subject,
-
-    text,
-  });
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        text,
+    });
 };
 
 module.exports = sendEmail;
-
-
