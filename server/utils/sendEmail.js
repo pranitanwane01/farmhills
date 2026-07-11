cd44c9d053e504058001b290a7a00d4bde1c7984
PRANIT ANWANE <pranitanwane@PRANITs-MacBook-Air.local>
2026-07-11 23:41:12 +0530

    last try

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -25,34 +25,28 @@
 // module.exports = sendEmail;
 
 
-
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-
-    tls: {
-        rejectUnauthorized: false,
-    },
+  host: "smtp-relay.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 const sendEmail = async (to, subject, text) => {
-    await transporter.verify();
-    console.log("SMTP Connected");
+  await transporter.verify();
+  console.log("SMTP Connected");
 
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        text,
-    });
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  });
 };
 
 module.exports = sendEmail;
